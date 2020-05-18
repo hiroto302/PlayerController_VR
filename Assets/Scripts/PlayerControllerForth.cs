a286ddfe57781529a86edb386b750e27e7cf9c00
hiroto302 <i.am.a.hiroto@gmail.com>
2020-05-18 22:12:48 +0900

    trasform.Translateによる移動機能実装

---
+++ Assets/Scripts/PlayerControllerForth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// オキュラスクエスト用 transform.TranslateによるPlayerの移動方法
+public class PlayerControllerForth : MonoBehaviour
+{
+    Rigidbody rb;
+    private float angleSpeed = 30.0f;
+    private float moveSpeed = 2.25f;
+    float moveForceMultiplier = 500;
+    [SerializeField] GameObject moveTarget;
+    // オキュラスクエストの入力値変数
+
+    float x, y;
+    Vector3 move;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+    // 右スティック入力・操作 方向
+        Vector3 rightStick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        x = rightStick.x;
+        y = rightStick.y;
+        // x と z 軸のみの変更
+        Vector3 direction = new Vector3( x , rb.position.y , y);
+        move = moveSpeed * Time.deltaTime * (direction.x * moveTarget.transform.right.normalized + direction.z * moveTarget.transform.forward.normalized);
+        transform.Translate(move, Space.World);
+
+        // 左スティック操作 角度
+        Vector3 leftStick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        Vector3 angle = new Vector3( 0, leftStick.x, 0);
+        transform.Rotate(angle * angleSpeed * Time.deltaTime);
+    }
+}
